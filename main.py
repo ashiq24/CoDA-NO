f4018e22a623143477a70eda9e176985c110fbc4
Md Rahman <mdashiqurr@selene-login-01.nvidia.com>
2023-09-17 11:58:56 -0700

    initial

--- main.py
+++ main.py
@@ -55,6 +55,6 @@ summary(model, (params.var_num*params.in_token_codim_en, 100, 100))
 ssl_model = SslWrapper(params, encoder, decoder, contrastive, predictor, stage='ssl')
 
 ssl_model = ssl_model.cuda()
-y = ssl_model(torch.randn(2,params.var_num*params.in_token_codim_en, 100, 100).cuda())
+y,_,_,_ = ssl_model(torch.randn(2,params.var_num*params.in_token_codim_en, 100, 100).cuda())
 
 print(y.shape)
