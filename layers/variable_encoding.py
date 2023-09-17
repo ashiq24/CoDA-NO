d13a50fa4a5dd6b7f81eb97e995d34ff9fa1326d
Md Rahman <mdashiqurr@selene-login-01.nvidia.com>
2023-09-17 08:49:35 -0700

    modules added

---
+++ layers/variable_encoding.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torch_harmonics as th
+
+class VaribaleEncoding2d(nn.Module):
+    def __init__(self, channel, mode_x, mode_y, basis='fft') -> None:
+        super().__init__()
+        self.coefficients_r = nn.Parameter(torch.empty(channel, mode_x, mode_y))
+        self.coefficients_i = nn.Parameter(torch.empty(channel, mode_x, mode_y))
+        self.reset_parameter()
+        self.mode_x = mode_x
+        self.mode_y = mode_y
+
+        self.basis = basis
+        if basis == 'fft':
+            self.transform = torch.fft.ifft2
+        elif basis == 'sht':
+            self.transform = th.InverseRealSHT(mode_x, mode_y, lmax=mode_x, mmax=mode_y, grid="legendre-gauss", norm="backward")
+        
+    def reset_parameter(self):
+        torch.nn.init.normal_(self.coefficients_r, mean=0.0, std=1)
+        torch.nn.init.normal_(self.coefficients_i, mean=0.0, std=1)
+    
+    def forward(self, x):
+        '''
+        it take a resolution and outputs the positional encodings
+        '''
+        size_x, size_y = x.shape[-2], x.shape[-1]
+        if self.basis == 'sht' and (self.transform.nlat != size_x or self.transform.nlon != size_y) :
+                self.transform = th.InverseRealSHT(size_x, size_y, lmax=self.mode_x, mmax= self.mode_y, \
+                                                grid = "legendre-gauss", norm= 'backward').to(self.coefficients_i.device, dtype=self.coefficients_i.dtype)
+        if self.basis == 'sht':
+            return self.transform(self.coefficients_r+ 1.0j*self.coefficients_i)
+        else:
+            return self.transform(self.coefficients_r+ 1.0j*self.coefficients_i, s=(size_x,size_y)).real
+        
+
---
+++ main.py
@@ -0,0 +1,37 @@
+from models.codano import CodANO
+from layers.attention import TnoBlock2d
+from layers.fino import SpectralConvKernel2d
+from torchsummary import summary
+
+
+token_codim = 1
+out_token_coken = 1
+hidden_token_codim = 4
+lifting_token_codim = 4
+n_heads = [2,2,2,2]
+scaling = [[1,1],[1,1],[1,1],[1,1]]
+modes = [[100,100],[100,100],[100,100],[100,100]]
+lifting = True
+projection = True
+operator_block = TnoBlock2d
+integral_operator = partial(SpectralConvKernel2d, frequency_mixer = False, fft_type='fft')
+integral_operator_top = integral_operator
+integral_operator_bottom = integral_operator
+
+var_encoding=True, #b
+var_num=10, # denotes the number of varibales
+var_enco_basis='fft',
+var_enco_channels=1,
+enable_cls_token=True,
+
+model = CodANO(in_token_codim=token_codim, hidden_token_codim=hidden_token_codim, lifting_token_codim=lifting_token_codim,\
+                n_layers=4, n_heads=n_heads, n_modes=modes, scalings=scaling, integral_operator=int_op,\
+                integral_operator_top=int_op_top,integral_operator_bottom=int_op_bottom,\
+                var_encoding=var_encoding,
+                var_enco_channels = var_enco_channels,
+                var_num = var_num,
+                enable_cls_token = enable_cls_token)
+
+summary(model, (var_num*token_codim, 100, 100)) 
+
+
