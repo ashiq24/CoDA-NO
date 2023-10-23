2707ccdfdec2361c280d7bd4110c59d90d078249
ashiq24 <ashiqbuet14@gmail.com>
2023-10-23 16:25:01 -0400

    data aug support

--- data_utils/data_utils.py
+++ data_utils/data_utils.py
@@ -99,7 +99,7 @@ class MakserNonuniformMest(object):
             max_location = max_blocks
             while n_drop_pix >0:
                 j = random.randint(0, self.in_nbr.shape[0])
-                mask[i,self.in_nbr[j]==1] = 0
+                mask[self.in_nbr[j]==1, i] = 0
                 n_drop_pix -= sum(self.in_nbr[j]).float()
                 max_location -= 1
                 if max_location == 0:
