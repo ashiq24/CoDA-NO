af5e230dfe82908bb4109816301929068df29ad6
ashiq24 <ashiqbuet14@gmail.com>
2023-10-25 04:19:48 -0400

    data aug support

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ from YParams import YParams
 from models.get_models import get_ssl_models_Gino, SslWrapper
 import os
 import torch
-
+from train.trainer import simple_trainer
+from data_utils.data_loaders import get_onestep_dataloader
 
 ## SSL model 
-params = YParams('./config/ssl.yaml', 'base_config', print_params=True)
+params = YParams('./config/ssl.yaml', 'gino', print_params=True)
 encoder, decoder, contrastive, predictor = get_ssl_models_Gino(params)
 
-ssl_model = SslWrapper(params, encoder, decoder, contrastive, predictor, stage='ssl')
+model = SslWrapper(params, encoder, decoder, contrastive, predictor, stage='ssl')
 
-ssl_model = ssl_model.cuda()
-y,_,_,_ = ssl_model(torch.randn(2,params.var_num*params.in_token_codim_en, 100, 100).cuda())
+model = model.cuda()
+train, test = get_onestep_dataloader()
+simple_trainer(model.cuda(), train, test, params)
 
-print(y.shape)
+model.stage = 'sl'
+simple_trainer(model.cuda(), train, test, params)
