093ecc5ae7d0d6175b1367a74690452c1e16640f
ashiq24 <ashiqbuet14@gmail.com>
2023-10-25 02:31:24 -0400

    data aug support

---
+++ data_utils/data_loaders.py
@@ -0,0 +1,31 @@
+import pickle
+import torch
+import random
+
+def get_onestep_dataloader(train_test_split=0.2, location='../Data/MP_data/', batch_size=1):
+    with open(location+'displacements0-5000.pkl', 'rb') as file:
+        displacements = pickle.load(file)
+    with open(location+'pressures0-5000.pkl', 'rb') as file:
+        pressure = pickle.load(file)
+    with open(location+'velocitoes0-5000.pkl', 'rb') as file:
+        velocities = pickle.load(file)
+    
+    combined = torch.cat([displacements,velocities,pressure], dim= -1)
+    step_t0 = combined[:-1, ...]
+    step_t1 =  combined[1:, ...]
+
+    indexs = [i for i in range(step_t0.shape[0])]
+    ntrain = int(train_test_split*len(indexs))
+    random.shuffle(indexs)
+    train_t0,test_t0 = step_t0[indexs[:ntrain]], step_t0[indexs[ntrain:]]
+    train_t1,test_t1 = step_t1[indexs[:ntrain]], step_t1[indexs[ntrain:]]
+
+    train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(train_t0, train_t1),\
+                                           batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(test_t0, test_t1),\
+                                           batch_size=batch_size, shuffle=False)
+    
+    return train_loader, test_loader
+
+
+        
