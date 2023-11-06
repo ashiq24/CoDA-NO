c5d1e49dcb4649897854d9c9bf772c852ebd0058
Mogab Elleithy <76572666+m4e7@users.noreply.github.com>
2023-11-06 01:01:37 -0800

    Add 'SWEDataset' to 'data_utils/'

--- data_utils/hdf5_datasets.py
+++ data_utils/hdf5_datasets.py
@@ -20,9 +20,119 @@ class Pair(NamedTuple):
     """Ground-truth tensor against model's prediction."""
 
 
+class SWEDataset:
+    """
+    Represents one HDF5 dataset of 2D Shallow Water Equation trajectories
+    from PDEBench.
+
+    For each dataset on .h5 file:
+    data=<HDF5 dataset "data": shape (101, 128, 128, 2), type "<f4">
+    with shape (T, W, H, C), channels=["activator", "inhibitor"]
+    T is linearly spaced in [0.0, 5.0], inclusive, with steps=101 and step_size=0.05.
+    X and Y are between: [-0.9941406, 0.9902344]
+    """
+
+    TRAJECTORY_LENGTH = 10
+    """Allow for 5 frames in input and 5 in output."""
+
+    TIME_DURATION = 101
+    """Each sample contains 101 time frames."""
+
+    SAMPLE_SIZE = 1000
+    """Each file contains 1,000 samples."""
+
+    def __init__(
+        self,
+        path: str,
+        train_test_split=1.0,
+        subsampling_rate=None,
+        stride=TRAJECTORY_LENGTH,
+        offset=0,
+    ):
+        self.path = path
+        self.file = h5py.File(path)
+        self._subsampling_rate = subsampling_rate
+        self.stride = stride
+        self.offset = offset
+
+        CLS = self.__class__
+        self.items_per_sample: int = (
+            (CLS.TIME_DURATION - self.offset) //
+            (CLS.TRAJECTORY_LENGTH + self.stride))
+        self.len = self.items_per_sample * np.floor(CLS.SAMPLE_SIZE * train_test_split)
+
+        # expect strings like: "0000", ..., "0999"
+        self.samples = list(self.file.keys())
+
+    @property
+    def subsampling_rate(self):
+        return (
+            self._subsampling_rate
+            if self._subsampling_rate is not None
+            else 1
+        )
+
+    @subsampling_rate.setter
+    def subsampling_rate(self, value):
+        self._subsampling_rate = value
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index) -> Pair:
+        if index >= self.len:
+            raise IndexError(f"Cannot access item {index} of {self.len}")
+
+        CLS = self.__class__
+        sample_idx, local_idx = divmod(index, self.items_per_sample)
+        time_idx = self.offset + local_idx * (CLS.TRAJECTORY_LENGTH + self.stride)
+
+        try:
+            # print(self.files[file_idx], sample_idx, time_idx, index)
+            sample = self._reconstruct_sample(
+                self.file,
+                sample_idx,
+                time_idx,
+                t_steps=CLS.TRAJECTORY_LENGTH,
+            )
+        except:
+            raise RuntimeError(
+                f'Failed to reconstruct sample for file {self.path} '
+                f'sample {sample_idx} time {time_idx}')
+
+        mid = CLS.TRAJECTORY_LENGTH // 2
+        return Pair(torch.as_tensor(sample[:mid]), torch.as_tensor(sample[mid:]))
+
+    def _reconstruct_sample(
+        self,
+        h5_file,
+        sample_idx,
+        time_idx,
+        t_steps,
+    ) -> np.ndarray:
+        """
+        Retrieves a sample from a SWE trajectory.
+
+        Reconstructs a sample from the target H5 file from times
+        ``[time_idx, time_idx + t_steps)`` (inclusive/exclusive, respectively).
+
+        Shape: (time, x, y, channel)
+        NOTE: channel is a singleton dimension in this case representing only
+          water depth.
+        """
+        sample_key = self.samples[sample_idx]
+        _slice = [
+            slice(time_idx, time_idx + t_steps),
+            slice(None, None, self.subsampling_rate),
+            slice(None, None, self.subsampling_rate),
+        ]
+
+        return h5_file[sample_key]['data'][_slice]
+
+
 class DiffusionReaction2DDataset:
     """
-    Represents one HDF5 dataset of a 2D Diffusion-Reaction trajectory from PDEBench.
+    Represents one HDF5 dataset of 2D Diffusion-Reaction trajectories from PDEBench.
 
     For each dataset on .h5 file:
     data=<HDF5 dataset "data": shape (101, 128, 128, 2), type "<f4">
