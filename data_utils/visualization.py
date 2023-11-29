b5a87f59d25303141927d42b593ff8997e04bbbf
Mogab Elleithy <76572666+m4e7@users.noreply.github.com>
2023-11-28 21:23:16 -0800

    Refactor 'show_multi_physics_data_diffs()' to vis.py

--- data_utils/visualization.py
+++ data_utils/visualization.py
@@ -129,3 +129,72 @@ def show_data_diff(
     plt.tight_layout()
     plt.show()
 
+
+def show_multi_physics_data_diffs(
+    model: Union[CodANO, CoDANOTemporal],
+    data_loader: data.DataLoader,
+    stage: Optional[StageEnum] = None,
+):
+    swe_loss, diff_loss, ns_loss = get_multi_physics_data_losses(
+        model,
+        data_loader,
+        # TODO add these indexes to config
+        ((0, 125), (125, 250), (250, 300)),
+        stage=stage,
+        logger=logger,
+    )
+
+    print("WATER DEPTH (SHALLOW WATER)")
+    (depth, _), _ = data_loader.dataset[swe_loss[0]]
+    show_data_diff(
+        depth,
+        swe_loss[2],
+        channel=0,
+        logger=logger,
+    )
+    
+    print("ACTIVATOR (DIFFUSION-REACTION)")
+    (activator, _), _ = data_loader.dataset[diff_loss[0]]
+    show_data_diff(
+        activator,
+        diff_loss[2],
+        channel=1,
+        logger=logger,
+    )
+    
+    print("INHIBITOR (DIFFUSION-REACTION)")
+    (inhibitor, _), _ = data_loader.dataset[diff_loss[0]]
+    show_data_diff(
+        inhibitor,
+        diff_loss[2],
+        channel=2,
+        logger=logger,
+    )
+    
+    print("PARTICLE DENSITY (NAVIER-STOKES)")
+    (particles, _), _ = data_loader.dataset[ns_loss[0]]
+    show_data_diff(
+        particles,
+        ns_loss[2],
+        channel=3,
+        logger=logger,
+    )
+    
+    print("X-VELOCITY (NAVIER-STOKES)")
+    (velocity_x, _), _ = data_loader.dataset[ns_loss[0]]
+    show_data_diff(
+        velocity_x,
+        ns_loss[2],
+        channel=4,
+        logger=logger,
+    )
+    
+    print("Y-VELOCITY (NAVIER-STOKES)")
+    (velocity_y, _), _ = data_loader.dataset[ns_loss[0]]
+    show_data_diff(
+        velocity_y,
+        ns_loss[2],
+        channel=5,
+        logger=logger,
+    )
+
--- pdebench.py
+++ pdebench.py
@@ -23,7 +23,7 @@ from YParams import YParams
 ## SSL model 
 # params = YParams('./config/ssl.yaml', 'codano_gino', print_params=True)
 # params = YParams('./config/test.yaml', 'codano_gino', print_params=True)
-params = YParams('./config/pdebench.yaml', 'codano_gino', print_params=True)
+params = YParams('./config/pdebench.yaml', 'codano_gino', print_params=False)
 verbose = True
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, force=True)
@@ -73,10 +73,10 @@ elif params.nettype == 'simple':
     model = get_model_fno(params)
 
 model = model.cuda()
-# -
 
 
-print(model)
+# +
+# print(model)
 
 # +
 # make it smol
@@ -166,9 +166,14 @@ test_predictive_loader = data.DataLoader(
     batch_size=params.batch_size,
     shuffle=False,
 )
+
+# +
+# import pdb; pdb.set_trace()
+# x, y = train_reconstructive_loader.dataset[150]
 # -
 
 # Train/test for the reconstructive (i.e. SSL) task:
+model.train()
 model.stage = StageEnum.RECONSTRUCTIVE
 # import pdb; pdb.set_trace()
 multi_physics_trainer(
@@ -177,9 +182,11 @@ multi_physics_trainer(
     test_reconstructive_loader,
     nn.MSELoss(),  # training loss_fn
     params,
+    # epochs=params.epochs // 10,
     wandb_log=params.wandb['log'],
     # wandb_log=False,  # debug
     log_interval=params.wandb['log_interval'],
+    # log_interval=1,
     script=False,
 )
 
@@ -226,76 +233,11 @@ test_single_physics(
 # *Investigate and visualize performance of encoder/decoder reconstruction.*
 
 
-# +
-
-# +
-def show_multi_physics_data_diffs(
-    model: Union[CodANO, CoDANOTemporal],
-    data_loader: data.DataLoader,
-    stage: Optional[StageEnum] = None,
-):
-    swe_loss, diff_loss, ns_loss = get_multi_physics_data_losses(
-        model,
-        data_loader,
-        ((0, 125), (125, 250), (250, 300)),
-        stage=stage,
-        logger=logger,
-    )
-
-    print("WATER DEPTH (SHALLOW WATER)")
-    show_data_diff(
-        data_loader.dataset[swe_loss[0]],
-        swe_loss[2],
-        channel=0,
-        logger=logger,
-    )
-    
-    print("ACTIVATOR (DIFFUSION-REACTION)")
-    show_data_diff(
-        data_loader.dataset[diff_loss[0]],
-        diff_loss[2],
-        channel=1,
-        logger=logger,
-    )
-    
-    print("INHIBITOR (DIFFUSION-REACTION)")
-    show_data_diff(
-        data_loader.dataset[diff_loss[0]],
-        diff_loss[2],
-        channel=2,
-        logger=logger,
-    )
-    
-    print("PARTICLE DENSITY (NAVIER-STOKES)")
-    show_data_diff(
-        data_loader.dataset[ns_loss[0]],
-        ns_loss[2],
-        channel=3,
-        logger=logger,
-    )
-    
-    print("X-VELOCITY (NAVIER-STOKES)")
-    show_data_diff(
-        data_loader.dataset[ns_loss[0]],
-        ns_loss[2],
-        channel=4,
-        logger=logger,
-    )
-    
-    print("Y-VELOCITY (NAVIER-STOKES)")
-    show_data_diff(
-        data_loader.dataset[ns_loss[0]],
-        ns_loss[2],
-        channel=5,
-        logger=logger,
-    )
-
 show_multi_physics_data_diffs(
     model,
     train_reconstructive_loader,
     StageEnum.RECONSTRUCTIVE,
 )
-# -
 
 show_multi_physics_data_diffs(
     model,
@@ -307,11 +249,13 @@ show_multi_physics_data_diffs(
 #
 # `torch.save(model, 'weights/model_4_ssl.pth')`
 
+model.train()
 logger.setLevel(logging.DEBUG)
 print(f"{params.pretrain_ssl=}")
 if params.pretrain_ssl:
     # Now train/test for the predictive (i.e. SL) task:
     model.stage = StageEnum.PREDICTIVE
+    # import pdb; pdb.set_trace()
     multi_physics_trainer(
         model,
         train_predictive_loader,
@@ -358,19 +302,20 @@ test_single_physics(
 )
 # print("Test on the mixed dataset:")
 # test_single_physics(0, 4400)
+# -
 
-# +
 show_multi_physics_data_diffs(
     model,
     train_predictive_loader,
     StageEnum.PREDICTIVE,
 )
 
-show_multi_physics_data_diffs(
-    model,
-    test_predictive_loader,
-    StageEnum.PREDICTIVE,
-)
+# +
+# show_multi_physics_data_diffs(
+#     model,
+#     test_predictive_loader,
+#     StageEnum.PREDICTIVE,
+# )
 # -
 
 # torch.save(model, 'weights/model_4_sl.pth')
