014d976ccc298af5418de163f74f175a01b81855
Mogab Elleithy <76572666+m4e7@users.noreply.github.com>
2024-01-10 10:22:39 -0800

    Add training/debugging helpers to `utils.py`
    * Add `save_model()` to write out intermediate models in training.
    * Add `signal_my_processes()` to terminate greedy processes while
      debugging/training

--- utils.py
+++ utils.py
@@ -1,9 +1,21 @@
+import datetime
+import logging
 import os
+import pathlib
+import psutil
+import re
+import signal
+
+from typing import List
+
+import h5py
+import haikunator
+import numpy as np
+import psutil
 import torch
 import torch.nn as nn
-import h5py
-import numpy as np
 
+HAIKU = haikunator.Haikunator()
 
 def get_wandb_api_key(api_key_file="config/wandb_api_key.txt"):
     try:
@@ -67,3 +79,117 @@ def get_mesh(location):
     h5f = h5py.File(location, 'r')
     mesh = h5f['mesh/coordinates']
     return mesh[:]
+
+
+# TODO add collision checks
+# TODO add opts to toggle haiku and date fixes
+def save_model(model, directory: pathlib.Path, stage=None, sep='_'):
+    """Saves a model with a unique prefix/suffix
+
+    The model is prefixed with is date (formatted like YYMMDDHHmm)
+    and suffixed with a "Heroku-like" name (for verbal reference).
+
+    Params:
+    ---
+    stage: None | StageEnum
+        Controls the infix of the model name according to the following mapping:
+        | None -> "model"
+        | RECONSTRUCTIVE -> "reconstructive"
+        | PREDICTIVE -> "predictive"
+    """
+    prefix = datetime.datetime.utcnow().strftime("%y%m%d%H%M")
+    infix = "model"
+    if stage is not None:
+        infix = stage.value.lower()
+    suffix = HAIKU.haikunate(token_length=0, delimiter=sep)
+
+    torch.save(model.state_dict(), directory / f"{prefix}{sep}{infix}{sep}{suffix}.pth")
+
+
+def extract_pids(message) -> List[int]:
+    # Assume `message` has a preamble followed by a sequence of tokens like
+    # "Process \d+" with extra characters in between such tokens.
+    
+    pattern = re.compile("(Process \d+)")
+    # Contains "Process" tokens and extra characters, interleaved:
+    tokens = pattern.split(message)
+    # print('\n'.join(map(repr, zip(split[1::2], split[2::2]))))
+    
+    pattern2 = re.compile("(\d+)")
+    # print('\n'.join([repr((s, pattern2.search(t)[0])) for t in tokens[1::2]]))
+    pids = [int(pattern2.search(t)[0]) for t in tokens[1::2]]
+
+    return pids
+
+
+# https://psutil.readthedocs.io/en/latest/#kill-process-tree
+def signal_process_tree(
+    pid,
+    sig=signal.SIGTERM,
+    include_parent=True,
+    timeout=None,
+    on_terminate=None,
+    logger=None,
+):
+    """Kill a process tree (including grandchildren) with signal ``sig``
+    
+    Return a (gone, still_alive) tuple.
+
+    Parameters
+    ---
+    timeout: float
+        Time, in seconds, to wait on each signaled process.
+    on_terminate: Optional[Callable]
+        A callback function which is called as soon as a child terminates.
+        Optional.
+    """
+    assert pid != os.getpid(), "won't kill myself"
+    parent = psutil.Process(pid)
+    children = parent.children(recursive=True)
+    if include_parent:
+        children.append(parent)
+    if logger is None:
+        logger = logging.getLogger()
+
+    wait_children = []
+    for p in children:
+        try:
+            p.send_signal(sig)
+            wait_children.append(p)
+        except psutil.AccessDenied:
+            logger.error(f"Unable to terminate Process {pid} (AccessDenied)")
+        except psutil.NoSuchProcess:
+            pass
+
+    gone, alive = psutil.wait_procs(
+        wait_children,
+        timeout=timeout,
+        callback=on_terminate,
+    )
+    return (gone, alive)
+
+
+def signal_my_processes(
+    username,
+    pids,
+    sig=signal.SIGTERM,
+    logger=None,
+):
+    if logger is None:
+        logger = logging.getLogger()
+    my_pids = []
+    for pid in pids:
+        p = psutil.Process(pid)
+        with p.oneshot():
+            p = p.as_dict(attrs=["username", "status"])
+
+        # TODO add other states to the filter
+        if p["username"] == username and p["status"] == "sleeping":
+            my_pids.append(pid)
+        else:
+            _p = {"pid": pid, **p}
+            logger.warning(f"Cannot signal process: {_p}")
+
+    for my_pid in my_pids:
+        gone, alive = signal_process_tree(pid, sig, timeout=60, logger=logger)
+        logger.info(f"{gone=}, {alive=}")
