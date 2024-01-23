5a71f12949fbf3d69b51c4613cbca8f896282137
ashiq24 <ashiqbuet14@gmail.com>
2024-01-23 23:45:41 +0000

    experiment for deeponet

---
+++ deeponet_baseline copy.sh
@@ -0,0 +1,22 @@
+python main.py --config deeponet_NSES_1 --ntrain 100
+python main.py --config deeponet_NSES_5 --ntrain 100
+python main.py --config deeponet_NSES_10 --ntrain 100
+python main.py --config deeponet_NS_1 --ntrain 100
+python main.py --config deeponet_NS_5 --ntrain 100
+python main.py --config deeponet_NS_10 --ntrain 100
+
+python main.py --config deeponet_NSES_1 --ntrain 250
+python main.py --config deeponet_NSES_5 --ntrain 250
+python main.py --config deeponet_NSES_10 --ntrain 250
+python main.py --config deeponet_NS_1 --ntrain 250
+python main.py --config deeponet_NS_5 --ntrain 250
+python main.py --config deeponet_NS_10 --ntrain 250
+
+python main.py --config deeponet_NSES_1 --ntrain 500
+python main.py --config deeponet_NSES_5 --ntrain 500
+python main.py --config deeponet_NSES_10 --ntrain 500
+python main.py --config deeponet_NS_1 --ntrain 500
+python main.py --config deeponet_NS_5 --ntrain 500
+python main.py --config deeponet_NS_10 --ntrain 500
+
+
