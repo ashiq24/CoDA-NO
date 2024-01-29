ef83880cd797f0580ba9489d6edc8d0b0abe60e9
ashiq24 <ashiqbuet14@gmail.com>
2024-01-29 02:25:27 +0000

    msg

--- unet_baseline.sh
+++ unet_baseline.sh
@@ -19,9 +19,9 @@ python main.py --config unet_NS_1 --ntrain 50
 python main.py --config unet_NS_5 --ntrain 50
 python main.py --config unet_NS_10 --ntrain 50
 
-python main.py --config unet_NSES_1 --ntrain 100
-python main.py --config unet_NSES_5 --ntrain 100
-python main.py --config unet_NSES_10 --ntrain 100
-python main.py --config unet_NS_1 --ntrain 100
-python main.py --config unet_NS_5 --ntrain 100
-python main.py --config unet_NS_10 --ntrain 100
+# python main.py --config unet_NSES_1 --ntrain 100
+# python main.py --config unet_NSES_5 --ntrain 100
+# python main.py --config unet_NSES_10 --ntrain 100
+# python main.py --config unet_NS_1 --ntrain 100
+# python main.py --config unet_NS_5 --ntrain 100
+# python main.py --config unet_NS_10 --ntrain 100
